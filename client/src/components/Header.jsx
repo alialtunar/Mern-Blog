ff019aeddf8f44b53f5f7c428cc88f47aa76cf05
alialtunar <altunarali284@gmail.com>
2024-09-01 16:24:04 +0300

    complete dark mode funcinality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,16 @@
-import { Button, Navbar, TextInput } from "flowbite-react";
-import { Link,useLocation } from "react-router-dom";
+import { Avatar, Button, Dropdown, DropdownDivider, DropdownItem, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon } from "react-icons/fa";
+import { FaMoon,FaSun } from "react-icons/fa";
+import { useSelector,useDispatch } from "react-redux";
+import { toogleTheme } from "../redux/theme/themeSlice";
+
 
 function Header() {
-    const path = useLocation().pathname;
+  const path = useLocation().pathname;
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
+  const { theme } = useSelector((state) => state.theme);
   return (
     <Navbar classNNavbarame="border-b-2">
       <Link
@@ -29,29 +35,44 @@ function Header() {
       </Button>
 
       <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
-          <FaMoon />
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill onClick={()=> dispatch(toogleTheme())}>
+          {theme === 'light' ?  <FaSun />: <FaMoon />} 
         </Button>
-        <Link to="sign-in">
-          <Button gradientDuoTone='purpleToBlue' outline>
-            Sign In
-          </Button>
-        </Link>
-        <Navbar.Toggle/>
+        {currentUser ? (
+          <Dropdown arrowIcon={false} inline
+          label={<Avatar alt="user" img={currentUser.profilePicture} rounded/>}>
+            <Dropdown.Header> 
+              <span className="block text-sm">@{currentUser.username}</span>
+              <span className="block text-sm font-medium truncate">{currentUser.email}</span>
+            </Dropdown.Header>
+            <Link to={"/dashboard?tab=profile"}>
+             <DropdownItem>Profile</DropdownItem>
+            </Link>
+            <DropdownDivider/>
+            <DropdownItem>Sign Out</DropdownItem>
+          </Dropdown>
+        ) : (
+          <Link to="sign-in">
+            <Button gradientDuoTone="purpleToBlue" outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
 
+        <Navbar.Toggle />
       </div>
-      
+
       <Navbar.Collapse>
-            <Navbar.Link active={path ==="/"} as={'div'}>
-                <Link to='/'>Home</Link>
-            </Navbar.Link>
-            <Navbar.Link active={path ==="/about"} as={'div'}>
-                <Link to='/about'>About</Link>
-            </Navbar.Link>
-            <Navbar.Link active={path ==="/projects"} as={'div'}>
-                <Link to='/projects'>Projects</Link>
-            </Navbar.Link>
-        </Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to="/projects">Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
